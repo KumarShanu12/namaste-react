c15222367c222a56979b7a96c9ca07ed69597ebe
Shanu Developer <kshanu233@gmail.com>
2023-10-26 01:08:25 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("div",{ id: "div"},React.createElement("h1",{},"i m child"))
+// console.log(heading)
+        const root = ReactDOM.createRoot(document.getElementById("root"))
+        root.render(heading)
